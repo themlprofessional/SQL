d5b91d0f5d97eaaed086d1a58f60b093c5f3c74d
themlprofessional <117509273+themlprofessional@users.noreply.github.com>
2022-11-05 16:39:17 +0530

    Update CTE's.sql

--- CTE's.sql
+++ CTE's.sql
@@ -1,3 +1,25 @@
-select * from EmployeeDemographics;
+with CTE_Employee as (
+    select FirstName, LastName, Gender, Salary,
+    count(Gender) over (partition by Gender) as TotalGender,
+    avg(Salary) over (partition by Salary) as AvgSalary
+    from EmployeeDemographics emp
+    join EmployeeSalary sal 
+    on emp.EmployeeID = sal.EmployeeID 
+    where Salary > 4500
+)
 
-select * from EmployeeSalary;
+select FirstName, AvgSalary from CTE_Employee;
+
+-- Create Table EmployeeDemographics 
+-- (EmployeeID int, 
+-- FirstName varchar(50), 
+-- LastName varchar(50), 
+-- Age int, 
+-- Gender varchar(50)
+-- )
+
+-- Create Table EmployeeSalary 
+-- (EmployeeID int, 
+-- JobTitle varchar(50), 
+-- Salary int
+-- )
