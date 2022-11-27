ee3c4eef8cf0cab5fbea1870f8f2f1ebcabba3e2
themlprofessional <117509273+themlprofessional@users.noreply.github.com>
2022-11-27 13:35:54 +0530

    updated pivot and unpivot

--- Pivot and Unpivot.sql
+++ Pivot and Unpivot.sql
@@ -19,10 +19,58 @@ insert into StudentMarkTbl values (11,'Dhoni', 'Biology', 56)
 insert into StudentMarkTbl values (12,'Dhoni', 'Maths', 89)
 insert into StudentMarkTbl values (NULL,NULL, NULL, NULL)
 
+select * from StudentMarkTbl
 
+-- Pivot
 select * from
 (select StudentName, Subject, Mark
 from StudentMarkTbl) as First
 Pivot (sum(mark) for Subject in (Physics, Chemistry, Biology, Maths)) as PivotTable
 
+-- Unpivot
+select * from
+(select StudentName, Subject, Mark
+from StudentMarkTbl) as First
+Pivot (sum(mark) for Subject in (Physics, Chemistry, Biology, Maths)) as PivotTable
+Unpivot(
+Mark for Subject in (Physics, Chemistry, Biology, Maths)
+) as UnPivotTable
 
+---------------------------------------------------------------------
+
+
+Create Table geeksforgeeks 
+( 
+CourseName nvarchar(50), 
+CourseCategory nvarchar(50),
+Price int  
+) 
+
+Insert into geeksforgeeks  values('C', 'PROGRAMMING', 5000) 
+Insert into geeksforgeeks  values('JAVA', 'PROGRAMMING', 6000) 
+Insert into geeksforgeeks  values('PYTHON', 'PROGRAMMING', 8000) 
+Insert into geeksforgeeks  values('PLACEMENT 100', 'INTERVIEWPREPARATION', 5000) 
+
+SELECT * FROM geeksforgeeks
+
+-- Pivot
+select * from
+(select CourseName, CourseCategory, Price
+from geeksforgeeks) as First
+Pivot(
+    sum(Price) for CourseCategory in (PROGRAMMING, INTERVIEWPREPARATION)
+) as PivotTable
+
+
+-- Unpivot
+select * from
+(select CourseName, CourseCategory, Price
+from geeksforgeeks) as First
+Pivot(
+    sum(Price) for CourseCategory in (PROGRAMMING, INTERVIEWPREPARATION)
+) as PivotTable
+UNPIVOT 
+( 
+Price FOR CourseCategory IN (PROGRAMMING, INTERVIEWPREPARATION)
+) 
+AS UnpivotTable
--- window_functions.sql
+++ window_functions.sql
@@ -1,3 +1,5 @@
+-- incomplete
+
 -- select
 -- from
 -- where
